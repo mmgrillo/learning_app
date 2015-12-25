a47f4cf4ed84a642f43aaf4aa22791eb3c7b0283
mmgrillo <mmgrillo1@gmail.com>
2015-12-25 12:15:24 +0000

    Finished Static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,22 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | The Reference and Learning App"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | The Reference and Learning App"
+  end
+
+  test "should get about" do
+  	get :about
+  	assert_response :success
+  	assert_select "title", "About | The Reference and Learning App"
   end
 
 end
+
+#"assert_response :success" the get request gets a 200:OK
+#"assert_select "title", "About"" looks for a <title> in the HTML with the content of "About"
